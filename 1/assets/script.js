a1c563929d69b3eb313ac30e363b326b4835cdb5
asmeovv <murtazinaasya517@gmail.com>
2025-04-11 23:42:48 +0300

    Delete 1 directory

--- 1/assets/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.getElementById('burger');
-    const linksMenu = document.getElementById('links-menu');
-
-    burger.addEventListener('click', function() {
-        linksMenu.classList.toggle('active'); 
-    });
-});
-
